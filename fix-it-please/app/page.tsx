f74a3084f6fff6e76c112b1c7a323b1fb2de8d76
SambaranBanerjee <sanbaranb25@gmail.com>
2025-12-01 09:11:08 +0530

    Created initial files and folders, also added a root page and layout.

---
+++ fix-it-please/app/page.tsx
@@ -0,0 +1,12 @@
+
+export default function index() {
+  return (
+    <div>
+        <h1>Welcome to Fix It Please</h1>
+        <div>
+            <button>Login</button>
+            <button>Register</button>
+        </div>
+    </div>
+  )
+}
