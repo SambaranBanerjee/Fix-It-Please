57a68ccf5625af5a3dfce7a63b63ce3e4338830c
SambaranBanerjee <sanbaranb25@gmail.com>
2025-12-05 03:17:49 +0530

    Added the view query backend

--- fix-it-please/app/api/entry/route.ts
+++ fix-it-please/app/api/entry/route.ts
@@ -4,13 +4,10 @@ import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
     try {
-
         await dbConnect();
-
+        
         const { title, type, solution } = await request.json();
 
-        // We must check if 'type' is an array and if it has items.
-        // '!type' alone won't catch an empty array []
         if (!title || !solution || !type || (Array.isArray(type) && type.length === 0)) {
             return NextResponse.json(
                 { message: "Title, Solution, and at least one Type are required" },
@@ -39,5 +36,30 @@ export async function POST(request: Request) {
             { message: "Internal Server Error", error: error instanceof Error ? error.message : String(error) },
             { status: 500 }
         );  
+    }
+}
+
+export async function GET() {
+    try {
+        await dbConnect();
+        const entries = await Entry.find().sort({ createdAt: -1 });
+        if (entries.length === 0) {
+            return NextResponse.json(
+                {message: 'No entries found'},
+                {status: 404}
+            )
+        }
+        else {
+            return NextResponse.json(
+                { entries },
+                { status: 200 }
+            );
+        }
+    } catch (error) {
+        console.error("Error fetching entries:", error);
+        return NextResponse.json(
+            { message: "Internal Server Error", error: error instanceof Error ? error.message : String(error) },
+            { status: 500 }
+        );
     }
 }
