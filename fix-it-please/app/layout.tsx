ccd98efd7b42400614b55ede83bb830f18ddf87b
SambaranBanerjee <sanbaranb25@gmail.com>
2026-06-17 16:58:33 +0530

    added the global code for authorization

--- fix-it-please/app/layout.tsx
+++ fix-it-please/app/layout.tsx
@@ -1,13 +1,24 @@
+import type {
+    Metadata,
+} from 'next';
+
 import './globals.css'
+import AuthSessionProvider from '@/components/AuthSessionProvider';
+
+export const metadata: Metadata = {
+    title: "Fix-it-Please"
+}
 export default function RootLayout({
     children,
-}: {
+}: Readonly<{
     children: React.ReactNode
-}) {
+}>) {
     return (
         <html lang="en">
             <body className='min-h-screen w-full'>
-                {children}
+                <AuthSessionProvider>
+                    {children}
+                </AuthSessionProvider>
             </body>
         </html>
     );
